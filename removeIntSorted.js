f0f6fd4fd8b3da5852a415e5818b99f229d3c094
John Boi <jon0908776@gmail.com>
2022-08-31 20:58:25 -0400

    solved removeIntSorted for JS and sortColors

---
+++ removeIntSorted.js
@@ -0,0 +1,13 @@
+var removeDuplicates = function(nums) {
+    for (let i=0; i<(nums.length-1);i++){
+        console.log(i)
+        console.log(nums)
+        if (nums[i]===nums[i+1]){
+            nums.splice(nums[i+1],1)
+            i-=1;
+        }
+    }
+    return nums
+};
+
+console.log(removeDuplicates([1,1,2]))
